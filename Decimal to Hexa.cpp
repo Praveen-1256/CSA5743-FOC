9ae87c7f58a9b2ce09e3d5b0caf7264bbcfcfe08
Praveen-1256 <113414072+Praveen-1256@users.noreply.github.com>
2023-01-28 20:52:14 +0530

    Add files via upload

---
+++ Decimal to Hexa.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main() 
+{
+	printf("**********---------Decimal to Hexadecimal conversion---------**********\n\n");
+	long int decimalNumber;
+	printf("Enter any decimal number: ");
+	scanf("%d",&decimalNumber);
+	printf("Equivalent hexadecimal number is: %X",decimalNumber);
+	return 0;
+}
+
